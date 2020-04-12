822e9d497f9d045db8c854b46d9f5692e413b408
袁龙海 <il>
2020-04-12 08:13:38 +0800

    合并后端服务到分支文件夹下

---
+++ Service_Demo/my_ReactServiceApi/Controllers/TitleController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using my_ReactServiceApi.Dto;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace my_ReactServiceApi.Controllers
+{
+    [ApiController]
+    [EnableCors("Policy1")]
+    [Route("[controller]/todos")]
+    public class TitleController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetTitleTodos()
+        {
+            Thread.Sleep(3000);
+            return Ok(new List<TitleTodoOutput>
+            {
+                new TitleTodoOutput
+                {
+                    Id = 1,
+                    UserId = 1,
+                    Title = "起床",
+                    IsCompleted = true
+                },
+                new TitleTodoOutput
+                {
+                    Id = 2,
+                    UserId = 1,
+                    Title = "吃饭",
+                    IsCompleted = false
+                },
+                new TitleTodoOutput
+                {
+                    Id = 3,
+                    UserId = 1,
+                    Title = "学习",
+                    IsCompleted = false
+                },
+                new TitleTodoOutput
+                {
+                    Id = 4,
+                    UserId = 1,
+                    Title = "跑步",
+                    IsCompleted = false
+                },
+                new TitleTodoOutput
+                {
+                    Id = 5,
+                    UserId = 1,
+                    Title = "午睡",
+                    IsCompleted = true
+                },
+                new TitleTodoOutput
+                {
+                    Id = 6,
+                    UserId = 1,
+                    Title = "发呆",
+                    IsCompleted = true
+                },
+                new TitleTodoOutput
+                {
+                    Id = 7,
+                    UserId = 1,
+                    Title = "听歌",
+                    IsCompleted = false
+                },
+                new TitleTodoOutput
+                {
+                    Id = 8,
+                    UserId = 1,
+                    Title = "游戏",
+                    IsCompleted = false
+                },
+                new TitleTodoOutput
+                {
+                    Id = 9,
+                    UserId = 1,
+                    Title = "吃饭",
+                    IsCompleted = false
+                },
+                new TitleTodoOutput
+                {
+                    Id = 10,
+                    UserId = 1,
+                    Title = "学习",
+                    IsCompleted = true
+                }
+            });
+        }
+    }
+}
---
+++ Service_Demo/my_ReactServiceApi/Dto/TitleTodoOutput.cs
@@ -0,0 +1,10 @@
+﻿namespace my_ReactServiceApi.Dto
+{
+    public class TitleTodoOutput
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
