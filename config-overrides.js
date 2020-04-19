295597d6b760527bc95bd783a66fb7e76ddc767d
袁龙海 <il>
2020-04-19 19:52:30 +0800

    使用react-app-rewired定制化配置

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/*
+ *@file config-overrides.js
+ *@author
+ *基于customize和react-app-rewired的定制化配置文件
+ */
+
+//引入一些相关的方法
+const { override } = require("customize-cra");
+
+module.exports = override();
