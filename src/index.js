3f9b03da22d1155c55cbd740c7efad478f9a8aac
HvaiY <337646685@qq.com>
2020-05-24 18:52:39 +0800

    提交ReadMe

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
+const state = { name: "aa", code: "cc" };
+const newState = Object.assign({}, state);
+console.log(state, newState);
+
 render(<App />, document.querySelector("#root"));
